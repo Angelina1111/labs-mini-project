9c3c893ce91cba8bd80a315d68144a65707c67af
Angelina1111 <75123083+Angelina1111@users.noreply.github.com>
2023-03-11 20:14:38 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 import time
 
-def countdown(time_sec):
-    while time_sec:
-        mins, secs = divmod(time_sec, 60)
-        timeformat = '{:02d}:{:02d}'.format(mins, secs)
-        print(timeformat, end='\r')
-        time.sleep(1)
-        time_sec -= 1
-    print("stop bonus")
-
-
-countdown(11)
+def countdown():
+    mins = int(input("Enter minutes: "))
+    secs = int(input("Enter seconds: "))
+    rounds = int(input("Enter number of rounds: "))
+    
+    for r in range(rounds):
+        for t in range(mins * 60 + secs, 0, -1):
+            mins, secs = divmod(t, 60)
+            timer = '{:02d}:{:02d}'.format(mins, secs)
+            print("Round:", r+1, "Timer:", timer, end="\r")
+            time.sleep(1)
+            
+        print("Round:", r+1, "completed!")
+        
+    print("All rounds completed!")
+    
+countdown()
